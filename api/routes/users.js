7a65de331d2935d109c65f135b75724592d60be2
tduncan14 <nbatre1@yahoo.com>
2021-06-30 15:08:02 -0700

    keep moving forward:

--- api/routes/users.js
+++ api/routes/users.js
@@ -20,7 +20,7 @@ router.put('/:userId', async(req,res) =>{
 
                 const updatedUser = await User.findByIdAndUpdate(req.params.userId,{
                     $set:req.body
-                })
+                },{new:true})
 
                 res.status(200).json(updatedUser);
         
@@ -38,6 +38,38 @@ router.put('/:userId', async(req,res) =>{
     else{
         res.status(401).json({'You can only update your account':req.params.id});
     }
+
+})
+
+
+router.delete(':delete/id', async(req,res) =>{
+
+
+  try{
+
+     const deleteUser = await User.findByIdAndDelete(req.params.id,(err,use)=>{
+  
+         if(err){
+             console.log('cannot find user or already deleted')
+         }
+
+         else{
+             console.log('user is deleted')
+         }
+
+     })
+
+     res.status(200).json(deleteUser);
+
+  }
+
+  catch(err){
+
+
+    console.log(err)
+
+  }
+
 
 })
 
